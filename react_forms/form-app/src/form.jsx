7ef9e255e71b5fb68f194108692440c956d66d63
katiehan22 <katiehan22@gmail.com>
2022-10-17 14:14:02 -0700

    Add handleChange to form

--- react_forms/form-app/src/form.jsx
+++ react_forms/form-app/src/form.jsx
@@ -1,22 +1,47 @@
 import { useRef, useState } from "react";
 
 function Form() {
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    phoneType: "",
+    staff: "",
+    bio: "",
+    notifications: ""
+  });
+
+  const handleChange = (incomingKey) => {
+    return (e) => {
+      console.log(e);
+      console.log(incomingKey);
+      const userObj = Object.assign({}, user, {[incomingKey]: e.target.value});
+      setUser(userObj);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
     <>
-      <form>
-        <label for="name">Name: </label>
-        <input type="text" value="" name="name" placeholder="Name" id="name"/>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name: </label>
+        <input type="text" value={user.name} name="name" placeholder="Name" id="name" onChange={handleChange("name")}/>
         <br />
         <br />
-        <label for="email">Email: </label>
-        <input type="text" value="" name="email" placeholder="Email" id="email"/>
+        <label htmlFor="email">Email: </label>
+        <input type="text" value={user.email} name="email" placeholder="Email" id="email" onChange={handleChange("email")} />
         <br />
         <br />
-        <label for="phone-number">Phone Number: </label>
-        <input type="text" value="" name="phone-number" placeholder="Phone Number" id="phone-number"/>
+        <label htmlFor="phone-number">Phone Number: </label>
+        <input type="text" value={user.phoneNumber} name="phone-number" placeholder="Phone Number" id="phone-number" onChange={handleChange("phoneNumber")} />
         <br />
         <br />
-        <label for="phone-type">Phone type: </label>
+        <label htmlFor="phone-type">Phone type: </label>
         <select name="phone-type" id="phone-type">
           <option value="home">Home</option>
           <option value="work">Work</option>
@@ -24,18 +49,21 @@ function Form() {
         </select>
         <br />
         <br />
-        <label for="staff">Staff: </label>
+        <label htmlFor="staff">Staff: </label>
         <input type="radio" value="instructor" name="staff" placeholder="Staff" id="staff"/>Instructor
         <input type="radio" value="student" name="staff" placeholder="Staff" id="staff"/>Student
         <br />
         <br />
-        <label for="bio">Bio: </label>
-        <textarea name="bio" id="bio" value="" placeholder="Bio" cols="30" rows="10"></textarea>
+        <label htmlFor="bio">Bio: </label>
+        <textarea name="bio" id="bio" value={user.bio} placeholder="Bio" cols="30" rows="10" onChange={handleChange("bio")}></textarea>
         <br />
         <br />
-        <label for="notifications">Sign up for Notifications?</label>
+        <label htmlFor="notifications">Sign up for Notifications?</label>
         <input type="checkbox" id="notifications" name="notifications" value="yes"/>Yes
         <input type="checkbox" id="notifications" name="notifications" value="no"/>No
+        <br />
+        <br />
+        <button>Submit</button>
 
       </form>
     </>
